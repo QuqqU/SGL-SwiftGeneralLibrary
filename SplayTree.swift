b8b81a053c94f7e8d1019aab0a14f917e75a648c
QuqqU <rldnd913@naver.com>
2017-12-31 14:15:50 +0900

    splayTree - add delete function
    in splayTree.swift
    adding "delete" function that deletes certain node
    by JKU

--- SplayTree.swift
+++ SplayTree.swift
@@ -5,7 +5,7 @@ import Swift
 protocol SplayTreeProtocol: class {
     associatedtype T where T: Comparable
     
-    func splay(targe me: Node<T>)
+    func splay(target me: Node<T>)
     
 }
 
@@ -27,30 +27,31 @@ extension Node: Equatable {
     }
 }
 
-class SplayTree<T: Comparable>{
+class SplayTree<T: Comparable> : SplayTreeProtocol{
     var tree: Node<T>?
+    var size: Int = 0
     
     func rotate(target me: Node<T>) {
-        guard let parent = me.parent else { return }
+        guard let _myParent = me.parent else { return }
         
         let buffer: Node<T>?
-        if parent.left == me {
-            parent.left = me.right
+        if _myParent.left == me {
+            _myParent.left = me.right
             buffer = me.right
-            me.right = parent
+            me.right = _myParent
         }
         else {
-            parent.right = me.left
+            _myParent.right = me.left
             buffer = me.left
-            me.left = parent
+            me.left = _myParent
         }
         
-        if let _buffer = buffer { _buffer.parent = parent }
-        me.parent = parent.parent
-        parent.parent = me
+        if let _buffer = buffer { _buffer.parent = _myParent }
+        me.parent = _myParent.parent
+        _myParent.parent = me
         
         if me.parent == nil { tree = me }
-        else if parent == me.parent?.left { me.parent?.left = me }
+        else if _myParent == me.parent?.left { me.parent?.left = me }
         else { me.parent?.right = me }
     }
     
@@ -75,6 +76,7 @@ class SplayTree<T: Comparable>{
                         newNode.parent = p
                         p.left = newNode
                         splay(target: newNode)
+                        self.size += 1
                         return
                     }
                     p = _pLeft
@@ -85,13 +87,17 @@ class SplayTree<T: Comparable>{
                         newNode.parent = p
                         p.right = newNode
                         splay(target: newNode)
+                        self.size += 1
                         return
                     }
                     p = _pRight
                 }
             }
         }
-        else { tree = Node<T>(value: key) }
+        else {
+            tree = Node<T>(value: key)
+            self.size += 1
+        }
     }
     
     func find(value key: T) -> Bool {
@@ -99,21 +105,52 @@ class SplayTree<T: Comparable>{
         findNode : while true {
             if key == p.key { break }
             if key < p.key {
-                guard let pLeft = p.left else { break }
-                p = pLeft
+                guard let _pLeft = p.left else { break }
+                p = _pLeft
             }
             else {
-                guard let pRight = p.right else { break }
-                p = pRight
+                guard let _pRight = p.right else { break }
+                p = _pRight
             }
         }
         splay(target: p)
-        return key == p.key
+        return p.key == key
     }
     
-    
+    func delete(value key: T) {
+        if !find(value: key) { return }
+        
+        self.size -= 1
+        if self.size == 0 {
+            tree = nil
+            return
+        }
+
+        if tree?.left == nil {
+            tree = tree?.right
+            tree?.parent = nil
+        }
+        else if tree?.right == nil {
+            tree = tree?.left
+            tree?.parent = nil
+        }
+        else {
+            var rightMost = tree?.left
+            while rightMost?.right != nil { rightMost = rightMost?.right }
+            rightMost?.right = tree?.right
+            tree?.right?.parent = rightMost
+            
+            tree = tree?.left
+            tree?.parent = nil
+            
+            splay(target: rightMost!)
+        }
+    }
 }
 
+
+
+/*
 let a = SplayTree<Int>()
 a.insert(value: 50)
 a.insert(value: 30)
@@ -127,12 +164,28 @@ a.insert(value: 0)
 
 
 a.find(value: 44)
-//a.find(value: 25)
+a.find(value: 25)
 
+a.delete(value: 5)
+a.delete(value: 35)
+a.find(value: 20)
+
+
+a.insert(value: 55)
+a.delete(value: 50)
+
+a.insert(value: 1)
+a.insert(value: 11)
+ a.insert(value: 21)
+ a.insert(value: 31)
+ a.insert(value: 41)
+ a.insert(value: 51)
+ a.insert(value: 61)
+
+a.delete(value: 21)
 dump(a)
 
-
-
+*/
 
 
 
